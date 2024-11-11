fccb7f1a9d2297bd02f6e978485655d8fd016c8b
arsena97 <zivzivadzearsena72@gmail.com>
2024-11-11 20:44:34 +0400

    a

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.BackgroundColor = ConsoleColor.Red;
+Console.WriteLine("Whate is your name");
+Console.ReadLine();
+Console.BackgroundColor = ConsoleColor.Blue;
+Console.WriteLine("Whate is your age");
+Console.ReadLine();
